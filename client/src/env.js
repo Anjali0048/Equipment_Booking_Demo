2a08a35d251cf9d28db73afee9854f9f6792d3d5
Anjali Singh <105203288+Anjali0048@users.noreply.github.com>
2024-07-17 15:57:49 +0530

    APIURL updates

--- client/src/env.js
+++ client/src/env.js
@@ -1,3 +1,4 @@
-export const APIURL = "https://equip-demo-back.vercel.app/"
+export const APIURL = "https://equipment-booking-demo4.onrender.com"
+// export const APIURL = "https://equip-demo-back.vercel.app/"
 // export const APIURL = "https://equipment-booking-demo.vercel.app/"
 // export const APIURL = "http://localhost:3001"
